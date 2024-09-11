b3875fb8b1dfa766e2e84fde6af4a06a96d2dc91
Sergii Karpenko <serg.karpenko@gmail.com>
2024-09-11 09:36:29 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var stud1 = new Student("John");
+        Console.WriteLine($"Hello, {stud1.Name}!");
+    }
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,6 @@
+﻿namespace ConsoleApp1;
+
+public class Student(string name)
+{
+    public string Name { get; set; } = name;
+}
